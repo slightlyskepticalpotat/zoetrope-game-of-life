58b9f5858ea2d392d065e7b2ca0909089784324f
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-06-21 16:14:29 -0400

    speed improvements

--- game_of_life.py
+++ game_of_life.py
@@ -1,47 +1,32 @@
-# Don't Panic.
-
+import copy
 import sys
 
-def peers(x, y):
-    neighbours = 0
-    if 0 <= x + 1 <= rows - 1 and 0 <= y <= columns - 1 and board[x + 1][y] == 1:
-        neighbours += 1
-    if 0 <= x - 1 <= rows - 1 and 0 <= y <= columns - 1 and board[x - 1][y] == 1:
-        neighbours += 1
-    if 0 <= x <= rows - 1 and 0 <= y + 1 <= columns - 1 and board[x][y + 1] == 1:
-        neighbours += 1
-    if 0 <= x <= rows - 1 and 0 <= y - 1 <= columns - 1 and board[x][y - 1] == 1:
-        neighbours += 1
-    if 0 <= x + 1 <= rows - 1 and 0 <= y + 1 <= columns - 1 and board[x + 1][y + 1] == 1:
-        neighbours += 1
-    if 0 <= x + 1 <= rows - 1 and 0 <= y - 1 <= columns - 1 and board[x + 1][y - 1] == 1:
-        neighbours += 1
-    if 0 <= x - 1 <= rows - 1 and 0 <= y + 1 <= columns - 1 and board[x - 1][y + 1] == 1:
-        neighbours += 1
-    if 0 <= x - 1 <= rows - 1 and 0 <= y - 1 <= columns - 1 and board[x - 1][y - 1] == 1:
-        neighbours += 1
-    return neighbours
-
-rows, columns = int(input("Rows: ")), int(input("Columns: "))
-board = [[0] * columns for _ in range(rows)]
+rows = int(input("Rows: "))
+columns = int(input("Columns: "))
 iterations = int(input("Iterations: "))
-print("Enter each live cell at the beginning with the row and column number separated by a space (bottom left is 0, 0). Enter \"-1 -1\" to move on:")
+empty_board = [[0 for i in range(columns)] for j in range(rows)]
+board = copy.deepcopy(empty_board)
+sys.stdout.write("Space-separated starting cells (bottom left is 0 0), \"-1 -1\" to continue: \n")
 while True:
     x, y = map(int, input().split())
-    if x + y == -2:
+    if x == -1 and y == -1:
         break
     board[rows - x - 1][y] = 1
 
 for i in range(iterations):
-    new = [[0] * columns for _ in range(rows)]
+    new = copy.deepcopy(empty_board)
     for r in range(rows):
         for c in range(columns):
-            neighbours = peers(r, c)
+            neighbours = 0
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if 0 <= r + dx <= rows - 1 and 0 <= c + dy <= columns - 1 and board[r + dx][c + dy] and abs(dx) + abs(dy) > 0:
+                        neighbours += 1
             if board[r][c] == 0: # dead cell
-                if neighbours == 3: # new cell
-                    new[r][c] = 1 
+                if neighbours == 3:
+                    new[r][c] = 1
             else: # live cell
-                if neighbours == 2 or neighbours == 3: # surviving cell
+                if neighbours == 2 or neighbours == 3:
                     new[r][c] = 1
     board = new
     sys.stdout.write(f"Iteration {i + 1}\n")
